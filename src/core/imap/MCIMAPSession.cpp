07056c5d405073e7cb598e9170d8dbe0be1ebffe
qianweicheng <weicheng.andy@gmail.com>
2016-03-08 14:46:03 +0800

    improve encoding(2)

--- src/core/imap/MCIMAPSession.cpp
+++ src/core/imap/MCIMAPSession.cpp
@@ -844,7 +844,7 @@ void IMAPSession::login(ErrorCode * pError)
         response = MCSTR("");
         if (mImap->imap_response != NULL) {
 //            response = String::stringWithUTF8Characters(mImap->imap_response);
-            response = Data::dataWithBytes(mImap->imap_response, (unsigned int) strlen(mImap->imap_response))->stringWithDetectedCharset("utf-8", false);
+            response = Data::dataWithBytes(mImap->imap_response, (unsigned int) strlen(mImap->imap_response))->stringWithDetectedCharset(MCSTR("utf-8"), false);
         }
          MC_SAFE_REPLACE_COPY(String, mLoginResponse, response);
         if (response->locationOfString(MCSTR("not enabled for IMAP use")) != -1 || response->locationOfString(MCSTR("enable IMAP")) != -1) {
