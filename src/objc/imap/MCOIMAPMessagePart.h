b6c2a222b7b9ad13535980c3ad0a2fab8fca5d22
Weicheng <weicheng.andy@gmail.com>
2016-07-26 16:00:57 +0800

    remove partID and size from MCOIMAPMessagePart

--- src/objc/imap/MCOIMAPMessagePart.h
+++ src/objc/imap/MCOIMAPMessagePart.h
@@ -16,9 +16,7 @@
 
 @interface MCOIMAPMessagePart : MCOAbstractMessagePart <NSCoding>
 
-/** A part identifier is of the form 1.2.1*/
-@property (nonatomic, copy) NSString * partID;
-@property (nonatomic, nonatomic) unsigned int size;
+
 @end
 
 #endif
