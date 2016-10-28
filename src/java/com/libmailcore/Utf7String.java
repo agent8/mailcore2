133ebf8f9627e6b53d1b0a7c0b9c0346aecd46d1
yuanlida <champoo@126.com>
2016-10-28 20:42:43 +0800

    Utf7Java jni.

---
+++ src/java/com/libmailcore/Utf7String.java
@@ -0,0 +1,12 @@
+package com.libmailcore;
+public class Utf7String extends NativeObject {
+	public native static String encodeMutf7(String src);
+	public native static String decodeMutf7(String src);
+	public native static String flattenHTML(String src);
+	public native static String transferEncodingDecode(byte[] data, int encoding, String charset, boolean isHTML);
+
+	public native static String encodeRFC822Str(String src);
+	public native static String decodeRFC822Str(String src);
+
+	public native static String htmlEncodedString(String src);
+}
---
+++ src/java/native/com_libmailcore_Utf7String.cpp
@@ -0,0 +1,75 @@
+#include "com_libmailcore_Address.h"
+
+#include "TypesUtils.h"
+#include "JavaHandle.h"
+#include "MCDefines.h"
+#include "MCUtf7String.h"
+
+using namespace mailcore;
+
+#define nativeType Utf7String
+#define javaType nativeType
+
+JNIEXPORT jstring JNICALL Java_com_libmailcore_Utf7String_encodeMutf7
+  (JNIEnv * env, jclass cls, jstring src)
+{
+    MC_POOL_BEGIN;
+    jobject result = MC_TO_JAVA(Utf7String::jencodeMutf7(MC_FROM_JAVA(String, src)));
+    MC_POOL_END;
+    return (jstring)result;
+}
+
+JNIEXPORT jstring JNICALL Java_com_libmailcore_Utf7String_decodeMutf7
+  (JNIEnv * env, jclass cls, jstring src)
+{
+    MC_POOL_BEGIN;
+    jobject result = MC_TO_JAVA(Utf7String::jdecodeMutf7(MC_FROM_JAVA(String, src)));
+    MC_POOL_END;
+    return (jstring)result; 
+}
+
+JNIEXPORT jstring JNICALL Java_com_libmailcore_Utf7String_flattenHTML
+  (JNIEnv * env, jclass cls, jstring src)
+{
+	MC_POOL_BEGIN;
+    jobject result = MC_TO_JAVA(Utf7String::jflattenHTML(MC_FROM_JAVA(String, src)));
+    MC_POOL_END;
+    return (jstring)result; 
+}
+
+JNIEXPORT jstring JNICALL Java_com_libmailcore_Utf7String_transferEncodingDecode
+  (JNIEnv * env, jclass cls, jbyteArray data, jint encoding, jstring charset, jboolean isHtml)
+{
+    MC_POOL_BEGIN;
+    jobject result = MC_TO_JAVA(Utf7String::jtransferEncodingDecode(MC_FROM_JAVA(Data, data), \
+      (uint32_t)encoding,MC_FROM_JAVA(String, charset),(bool)isHtml));
+    MC_POOL_END;
+    return (jstring)result; 
+}
+
+JNIEXPORT jstring JNICALL Java_com_libmailcore_Utf7String_encodeRFC822Str
+  (JNIEnv * env, jclass cls, jstring src)
+{
+    MC_POOL_BEGIN;
+    jobject result = MC_TO_JAVA(Utf7String::jencodeRFC822Str(MC_FROM_JAVA(String, src)));
+    MC_POOL_END;
+    return (jstring)result; 
+}
+
+JNIEXPORT jstring JNICALL Java_com_libmailcore_Utf7String_decodeRFC822Str
+  (JNIEnv * env, jclass cls, jstring src)
+{
+    MC_POOL_BEGIN;
+    jobject result = MC_TO_JAVA(Utf7String::jdecodeRFC822Str(MC_FROM_JAVA(String, src)));
+    MC_POOL_END;
+    return (jstring)result; 
+}
+
+JNIEXPORT jstring JNICALL Java_com_libmailcore_Utf7String_htmlEncodedString
+  (JNIEnv * env, jclass cls, jstring src)
+{
+    MC_POOL_BEGIN;
+    jobject result = MC_TO_JAVA(Utf7String::jhtmlEncodedString(MC_FROM_JAVA(String, src)));
+    MC_POOL_END;
+    return (jstring)result; 
+}
