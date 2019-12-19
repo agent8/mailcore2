a6abc259f1caba98ca30cd8daf6c47c30995ad13
Shuhao Zhang <shuhao@easilydo.com>
2019-12-19 10:20:51 -0800

    cocoapod build script

--- scripts/edo-build-mailcore2-ios-cocoapod.sh
+++ scripts/edo-build-mailcore2-ios-cocoapod.sh
@@ -4,7 +4,7 @@ pushd "`dirname "$0"`" > /dev/null
 scriptpath="`pwd`"
 popd > /dev/null
 
-. "$scriptpath/include.sh/edo-build-dep.sh"
+. "$scriptpath/include.sh/edo-build-dep-cocoapod.sh"
 
 name="mailcore2-ios"
 xcode_target="edo Mailcore2"
@@ -12,6 +12,6 @@ xcode_project="mailcore2.xcodeproj"
 library="libEdoMailcore2.a"
 embedded_deps="ctemplate-ios libsasl-ios tidy-html5-ios"
 build_mailcore=1
-build_for_external=1
+TOPDIR="$scriptpath/.."
 
 build_git_ios
