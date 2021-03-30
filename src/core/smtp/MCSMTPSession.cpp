87bc0b823aea7840e09642f2d9efd2bfc795d51e
lzx0213104 <lzx0213104@gmail.com>
2021-03-30 14:56:32 +0800

    delete mailsmtp_rcpt from SMTPSession::checkAccount

--- src/core/smtp/MCSMTPSession.cpp
+++ src/core/smtp/MCSMTPSession.cpp
@@ -625,7 +625,7 @@ void SMTPSession::login(ErrorCode * pError)
 void SMTPSession::checkAccount(Address * from, ErrorCode * pError)
 {
     int r;
-    
+
     loginIfNeeded(pError);
     if (* pError != ErrorNone) {
         return;
@@ -641,19 +641,19 @@ void SMTPSession::checkAccount(Address * from, ErrorCode * pError)
         * pError = ErrorInvalidAccount;
         return;
     }
-    
-    r = mailsmtp_rcpt(mSmtp, "email@invalid.com");
-    saveLastResponse();
-    if (r == MAILSMTP_ERROR_STREAM) {
-        * pError = ErrorConnection;
-        mShouldDisconnect = true;
-        return;
-    }
-    else if (r != MAILSMTP_NO_ERROR) {
-        * pError = ErrorInvalidAccount;
-        return;
-    }
-    
+
+    // r = mailsmtp_rcpt(mSmtp, "email@invalid.com");
+    // saveLastResponse();
+    // if (r == MAILSMTP_ERROR_STREAM) {
+    //     * pError = ErrorConnection;
+    //     mShouldDisconnect = true;
+    //     return;
+    // }
+    // else if (r != MAILSMTP_NO_ERROR) {
+    //     * pError = ErrorInvalidAccount;
+    //     return;
+    // }
+
     * pError = ErrorNone;
 }
 
