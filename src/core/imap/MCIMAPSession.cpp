a1eb26b418e89abf1f06253d56bc01c16c0e1f6f
Zhixin Lu <zlu@yipitdata.com>
2022-03-30 17:53:40 +0800

    Try "list" command if "xlist" command failed in IMAPSession::fetchAllFolders

--- src/core/imap/MCIMAPSession.cpp
+++ src/core/imap/MCIMAPSession.cpp
@@ -1608,6 +1608,10 @@ Array * /* IMAPFolder */ IMAPSession::fetchAllFolders(ErrorCode * pError)
     
     if (mXListEnabled) {
         r = mailimap_xlist(mImap, MCUTF8(prefix), "*", &imap_folders);
+        if (r == MAILIMAP_ERROR_PARSE) {
+            clearStreamBufferOfLastCommand();
+            r = mailimap_list(mImap, MCUTF8(prefix), "*", &imap_folders);
+        }
     }
     else {
         r = mailimap_list(mImap, MCUTF8(prefix), "*", &imap_folders);
