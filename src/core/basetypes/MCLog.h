9e14de87539135572183b7c61756813a005665b4
Zhixin Lu <zlu@yipitdata.com>
2023-01-03 14:20:16 +0800

    Use MCHandleLog instead of MCLogInternal for MCLog on TARGET_OS_MAC

--- src/core/basetypes/MCLog.h
+++ src/core/basetypes/MCLog.h
@@ -8,7 +8,17 @@
 //zhaoshuo:
 //if log is disabled, should not calculate __VA_ARGS__ if it is a statement. like TypesUtils.cpp#arrayObjectToJavaConverter#MCUTF8(...)
 //some messages can't translate to UTF8 code, and may cause crash.
-#define MCLog(...) if(MCLogEnabled) MCLogInternal(NULL, __FILE__, __LINE__, 0, __VA_ARGS__)
+#if __APPLE__
+#   include "TargetConditionals.h"
+#   if defined(TARGET_OS_MAC) && TARGET_OS_MAC
+#       define MCLog(...) if(MCLogEnabled) MCHandleLog(__FILE__, __LINE__, __VA_ARGS__)
+#   else
+#       define MCLog(...) if(MCLogEnabled) MCLogInternal(NULL, __FILE__, __LINE__, 0, __VA_ARGS__)
+#   endif
+#else
+#   define MCLog(...) if(MCLogEnabled) MCLogInternal(NULL, __FILE__, __LINE__, 0, __VA_ARGS__)
+#endif
+
 #define MCLogStack(...) if(MCLogEnabled) MCLogInternal(NULL, __FILE__, __LINE__, 1, __VA_ARGS__)
 #define MCLogOutput(...) if(MCLogEnabled) MCHandleLog(__FILE__, __LINE__, __VA_ARGS__)
 
